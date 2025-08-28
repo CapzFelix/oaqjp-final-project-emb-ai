a9f723e87bbae4951f5a6f51e44d45b783699433
CapzFelix <felix@techfelix.io>
2025-08-28 08:10:34 -0400

    Task 3 Completed

--- emotion_detection.py
+++ emotion_detection.py
@@ -17,5 +17,20 @@ def emotion_detector(text_to_analyze):
     # Parse the response from the API
     formatted_response = json.loads(response.text)
 
-    # Print out the emotion detection result
-    print(formatted_response)
+    # Parse the specific emotion score into a dictionary
+    emotion_results = {
+        'anger': formatted_response['emotionPredictions'][0]['emotion']['anger'],
+        'disgust': formatted_response['emotionPredictions'][0]['emotion']['disgust'],
+        'fear': formatted_response['emotionPredictions'][0]['emotion']['fear'],
+        'joy': formatted_response['emotionPredictions'][0]['emotion']['joy'],
+        'sadness': formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        }
+
+    # Find the emotion with the highest score
+    max_key = max(emotion_results, key=emotion_results.get)
+
+    # Append the dominant emotion into the dictionary
+    emotion_results['dominant_emotion'] = max_key
+
+    # Return the dictionary result
+    return emotion_results
